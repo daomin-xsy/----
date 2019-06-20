ec836926c19e6417ab3052dcdb9a49efd2794e74
daomin-xsy <51433504+daomin-xsy@users.noreply.github.com>
2019-06-20 10:26:30 +0800

    文件传输
    ture

--- q/main.cpp
+++ q/main.cpp
@@ -1,15 +1,23 @@
-//
-//  main.cpp
-//  q
-//
-//  Created by s20181105303 on 2019/6/20.
-//  Copyright © 2019 s20181105303. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <fstream>
+using namespace std;
+int main() {
+    int a,b;
+    ofstream out("/Users/s20181105303/Desktop/测试一.txt");
+    ifstream in("/Users/s20181105303/Desktop/测试二.txt");
+    if(in.is_open())
+    {
+        in>>a>>b;
+        cout<<a<<"  "<<b<<endl;
+        in.close();
+    }
+    if(out.is_open())
+    {
+        out<<"c = "<<a+b<<endl;
+        cout<<"c = "<<a+b<<endl;
+        out.close();
+    }
+    
+    
     return 0;
 }
